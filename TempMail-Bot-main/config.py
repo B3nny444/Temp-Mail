d503fdeabe05ac134c6260243d3d12992aaecdac
Bunny <144216222+B3nny444@users.noreply.github.com>
2025-03-27 20:40:03 +0530

    Add files via upload

---
+++ TempMail-Bot-main/config.py
@@ -0,0 +1,7 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "27094161"  # Replace this API ID with your actual API ID
+API_HASH = "39477b23f5e6abea95fe0f92b7f00de0"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "7425029151:AAFSycqRhoQ2VuzSTb_ksYeGGUWPqwsXJN4"  # Replace this BOT_TOKEN
